d1b4391229abcccb3af190511f5ec33b67c23c2b
Mahmudul Hasan Emon <a67mahmudulhasanemon@gmail.com>
2026-06-10 20:22:30 +0600

    Initial commit

--- app/src/main/java/com/holystock/newmyoffer/activity/WelcomeActivity.java
+++ app/src/main/java/com/holystock/newmyoffer/activity/WelcomeActivity.java
@@ -23,8 +23,8 @@ public class WelcomeActivity extends BaseActivity {
         setContentView(R.layout.activity_welcome);
 
         new Handler().postDelayed(() -> {
-            //Intent intent = new Intent(this, com.holystock.newmyoffer.activity.login_or_signup.LoginActivity.class);
-            Intent intent = new Intent(this, RechargeNumberActivity.class);
+            Intent intent = new Intent(this, com.holystock.newmyoffer.activity.login_or_signup.LoginActivity.class);
+            //Intent intent = new Intent(this, RechargeNumberActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
